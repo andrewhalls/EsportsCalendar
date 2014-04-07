def33ac57acda88bdee8fa550a1e14c8f3d04d89
Andrew Halls <andrew.halls@netmatters.com>
2014-04-07 18:12:50 +0100

    Fix tests

--- app/tests/functional/CreateChannelCept.php
+++ app/tests/functional/CreateChannelCept.php
@@ -3,6 +3,6 @@ $scenario->group('admin');
 
 $I = new TestGuy($scenario);
 $I->wantTo('Create A New Channel');
-AdminBroadcastPage::of($I)
-    ->createBroadcast('Test Channel', 'URL', 'English');
+AdminChannelPage::of($I)
+    ->createChannel('Test Channel', 'URL', 'English');
 $I->see('Successfully Created Channel');
--- app/tests/functional/CreateGameCept.php
+++ app/tests/functional/CreateGameCept.php
@@ -3,6 +3,6 @@ $scenario->group('admin');
 
 $I = new TestGuy($scenario);
 $I->wantTo('Create A New Game');
-AdminGanePage::of($I)
+AdminGamePage::of($I)
     ->createGame('Test Games', 'URL');
 $I->see('Successfully Created Game');
